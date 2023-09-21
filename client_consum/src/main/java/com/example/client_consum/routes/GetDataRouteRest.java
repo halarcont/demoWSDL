e599480a9c03b656d94139dc419a2edd15e4637b
halarcont <herito9602@gmail.com>
2023-09-21 12:28:03 -0400

    Add Validations

--- client_consum/src/main/java/com/example/client_consum/routes/GetDataRouteRest.java
+++ client_consum/src/main/java/com/example/client_consum/routes/GetDataRouteRest.java
@@ -2,6 +2,7 @@ package com.example.client_consum.routes;
 
 import com.wsdlpackage.ClientRequest;
 import com.wsdlpackage.ClientResponse;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
@@ -14,6 +15,11 @@ public class GetDataRouteRest extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        onException(com.fasterxml.jackson.databind.exc.MismatchedInputException.class)
+                .handled(true)
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
+                .setBody().constant("El cuerpo de la solicitud estaba vacío o no era un JSON válido.");
+
         rest("/demo")
                 .bindingMode(RestBindingMode.off)
 
@@ -24,14 +30,20 @@ public class GetDataRouteRest extends RouteBuilder {
 
         from("direct:clientInfo")
                 .routeId("direct:clientInfo").noMessageHistory().noTracing().noStreamCaching()
+                .choice()
+                .when(body().isNotNull())
                 .unmarshal().json(JsonLibrary.Jackson, ClientRequest.class)
                 .setBody(exchange -> {
                     ClientRequest clientRequest = exchange.getIn().getBody(ClientRequest.class);
                     ClientResponse clientResponse = new ClientResponse();
                     clientResponse.setFullName(clientRequest.getName() + " " + clientRequest.getLastName());
-                    clientResponse.setPersonalData("Telefono: " + clientRequest.getTelephoneNumber() + " " + "Address: " + clientRequest.getAddress() + " " + "Email: "  + clientRequest.getEmail());
+                    clientResponse.setPersonalData("Telephone: " + clientRequest.getTelephoneNumber() + " " + "Address: " + clientRequest.getAddress() + " " + "Email: " + clientRequest.getEmail());
                     return clientResponse;
                 })
-                .marshal().json(JsonLibrary.Jackson, ClientResponse.class);
+                .marshal().json(JsonLibrary.Jackson, ClientResponse.class)
+                .endChoice()
+                .otherwise()
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
+                .setBody().constant("El cuerpo de la solicitud no puede estar vacío.");
     }
 }
